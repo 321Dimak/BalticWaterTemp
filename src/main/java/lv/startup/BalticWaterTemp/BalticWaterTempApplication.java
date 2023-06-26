91962b42c00fad48f5883fd776d8e5e387cd5636
Dmitrijs Kozlovs <dmitrijs.a.kozlovs@gmail.com>
2023-06-26 12:01:07 +0300

    Initial commit

---
+++ src/main/java/lv/startup/BalticWaterTemp/BalticWaterTempApplication.java
@@ -0,0 +1,13 @@
+package lv.startup.BalticWaterTemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalticWaterTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalticWaterTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/startup/BalticWaterTemp/BalticWaterTempApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.startup.BalticWaterTemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalticWaterTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
