2b8c5cfb2810cc5cbe6f4620711f0639cc3c31d3
Pavel Kleymenov <kleymennno@outlook.lv>
2023-08-17 09:34:28 +0300

    Removed roles, left security for future needs

--- src/main/java/lv/startup/BalticWaterTemp/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package lv.startup.BalticWaterTemp.repository;
-
-import lv.startup.BalticWaterTemp.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
