c1e6ac03df694727afb4a8bf689074f9577f38d6
Dmitrijs Kozlovs <dmitrijs.a.kozlovs@gmail.com>
2023-08-17 18:04:34 +0300

    Application test fix

--- src/test/java/SpringContextTest.java
+++ src/test/java/SpringContextTest.java
@@ -4,6 +4,7 @@ import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -12,11 +13,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @Disabled
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = SpringCoreConfiguration.class)
-@RequiredArgsConstructor
 public class SpringContextTest {
 
-
-    private final ApplicationContext context;
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     public void start() {
--- src/test/java/lv/startup/BalticWaterTemp/BalticWaterTempApplicationTests.java
+++ src/test/java/lv/startup/BalticWaterTemp/BalticWaterTempApplicationTests.java
@@ -1,10 +1,13 @@
 package lv.startup.BalticWaterTemp;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@Disabled
+//@Disabled
 @SpringBootTest
 class BalticWaterTempApplicationTests {
 
-
+    @Test
+    void contextLoads() {
+    }
 }
