c006141ae0f43bbffbf25180b59f210717362ea1
Marghoob Ahmad <marghoobahmd25@gmail.com>
2023-08-17 13:44:42 +0300

    1) Added simple API request for testing 2) Added Styling
    (cherry picked from commit ac6eb7ad0cceb26f649ad5a43a37bf5feaccb72b)

--- src/main/java/lv/startup/BalticWaterTemp/web_ui/config/SpringWebConfiguration.java
+++ src/main/java/lv/startup/BalticWaterTemp/web_ui/config/SpringWebConfiguration.java
@@ -3,14 +3,13 @@ package lv.startup.BalticWaterTemp.web_ui.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvcg
 @ComponentScan(basePackages = "lv.startup.BalticWaterTemp")
 public class SpringWebConfiguration implements WebMvcConfigurer {
 
