84b89c3a0d7380d85985dd9c13486ebc25ce4413
Marghoob Ahmad <marghoobahmd25@gmail.com>
2023-08-18 12:40:13 +0300

    Improved Homepage
    (cherry picked from commit 05e2327e4f4dc768e136a2facefb9127b3619aac)

--- src/main/java/lv/startup/BalticWaterTemp/core/services/ApiService.java
+++ src/main/java/lv/startup/BalticWaterTemp/core/services/ApiService.java
@@ -7,15 +7,16 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ApiService {
 
-    private final String API_BASE_URL = "https://data.gov.lv/dati/lv/api/3/action";
-
+    private final String API_BASE_URL = "https://data.gov.lv/dati/api/3/action/datastore_search_sql";
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public ResponseEntity<String> fetchDataFromApi() {
-        String resource_id = "a90de53b-e8b6-4cda-97c4-ecb86fbafc2d";
-        int limit = 5;
+    public ResponseEntity<String> fetchDataFromApi(String fromDate, String toDate) {
+        String sqlQuery = "SELECT * FROM \"a90de53b-e8b6-4cda-97c4-ecb86fbafc2d\" " +
+                "WHERE \"a90de53b-e8b6-4cda-97c4-ecb86fbafc2d\".\"ABBREVIATION\" = 'WTEMD' " +
+                "AND \"a90de53b-e8b6-4cda-97c4-ecb86fbafc2d\".\"STATION_ID\" = 'HD073720' " +
+                "AND \"a90de53b-e8b6-4cda-97c4-ecb86fbafc2d\".\"DATETIME\" BETWEEN '" + fromDate + "' AND '" + toDate + "'";
 
-        String url = API_BASE_URL + "/datastore_search?resource_id=" + resource_id + "&limit=" + limit;
+        String url = API_BASE_URL + "?sql=" + sqlQuery;
         return restTemplate.getForEntity(url, String.class);
     }
 }
