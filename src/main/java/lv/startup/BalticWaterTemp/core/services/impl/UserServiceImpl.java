ea05d8aee83b24c365f63313aace80f4ce5f41dd
Pavel Kleymenov <kleymennno@outlook.lv>
2023-08-22 11:57:09 +0300

    Email after register

--- src/main/java/lv/startup/BalticWaterTemp/core/services/impl/UserServiceImpl.java
+++ src/main/java/lv/startup/BalticWaterTemp/core/services/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package lv.startup.BalticWaterTemp.core.services.impl;
 
+import jakarta.mail.internet.MimeMessage;
 import lv.startup.BalticWaterTemp.core.database.UserRepository;
 import lv.startup.BalticWaterTemp.core.security.dto.UserDto;
 import lv.startup.BalticWaterTemp.core.entity.User;
 import lv.startup.BalticWaterTemp.core.services.UserService;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import org.springframework.mail.javamail.JavaMailSender;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,9 +23,12 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JavaMailSender mailSender;
     @Override
     public void saveUser(UserDto userDto) {
         JpaUserRepository.save(new User(userDto.getEmail(), userDto.getName(), passwordEncoder.encode(userDto.getPassword())));
+        sendWelcomeEmail(userDto);
     }
 
     @Override
@@ -39,5 +45,20 @@ public class UserServiceImpl implements UserService {
 
     private UserDto convertEntityToDto(User user) {
         return new UserDto(user.getEmail(), user.getUsername(), null);
+    }
+    private void sendWelcomeEmail(UserDto userDto) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            String htmlMsg = "<h3>Welcome " + userDto.getName() + "!</h3><p>Thank you for registering at BalticWaterTemp.</p>";
+            helper.setText(htmlMsg, true);  // set to true to enable HTML formatting
+            helper.setTo(userDto.getEmail());
+            helper.setSubject("Welcome to BalticWaterTemp");
+            helper.setFrom("noreply@balticwatertemp.com");  // replace with your sending email
+            mailSender.send(mimeMessage);
+        } catch (Exception e) {
+            // Log the exception for debugging. You might want to handle this differently based on your application's needs.
+            e.printStackTrace();
+        }
     }
 }
