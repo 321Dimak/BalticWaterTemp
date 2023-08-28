938cfa071b636b98e9370883f4db6b71ad62a773
Dmitrijs Kozlovs <dmitrijs.a.kozlovs@gmail.com>
2023-08-28 10:46:15 +0300

    User email filled automatic

--- src/main/java/lv/startup/BalticWaterTemp/web_ui/controllers/ThresholdSetterController.java
+++ src/main/java/lv/startup/BalticWaterTemp/web_ui/controllers/ThresholdSetterController.java
@@ -5,10 +5,13 @@ import lv.startup.BalticWaterTemp.core.responses.SaveTempNotificationResponse;
 import lv.startup.BalticWaterTemp.core.services.ThresholdSetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 public class ThresholdSetterController {
@@ -23,9 +26,12 @@ public class ThresholdSetterController {
     }
 
     @PostMapping("/thresholdSetter")
-    public String saveTempThreshold(
+    public String saveTempThreshold(Model model, Principal principal,
             @ModelAttribute(value = "request") TempThresholdDTO request,
-            ModelMap modelMap) {
+                                    ModelMap modelMap) {
+        String loggedInUserEmail = principal.getName();
+        model.addAttribute("userEmail", loggedInUserEmail);
+        request.setUserEmail(loggedInUserEmail);
         SaveTempNotificationResponse response = thresholdSetService.setTempThreshold(request);
         modelMap.addAttribute("message", response.getMessage());
 
